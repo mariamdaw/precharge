6e1bf3b82799f3d2193c9014c45c1bb9b063a7c0
Windows <Windows@DESKTOP-Q4F8P5R>
2026-06-28 02:18:13 +0300

    Add project files.

---
+++ mda take 2/Services/Interfaces/IConverterService.cs
@@ -0,0 +1,24 @@
+﻿using mda_take_2.Models;
+
+public interface IConverterService
+{
+    // State
+    ConverterState CurrentState { get; }
+    event EventHandler<ConverterState> StateChanged;
+
+    // PWM
+    Task SetPwmParametersAsync(int channelIndex, PwmChannel parameters);
+    Task<PwmChannel> GetPwmParametersAsync(int channelIndex);
+
+    // ADC
+    event EventHandler<IReadOnlyList<AdcChannel>> AdcUpdated;
+
+    // GPIO
+    Task SetGpioPinAsync(int pinIndex, bool value);
+    Task<IReadOnlyList<GpioPin>> GetGpioStatesAsync();
+
+    // Connection lifecycle
+    Task ConnectAsync();
+    Task DisconnectAsync();
+    bool IsConnected { get; }
+}
---
+++ mda take 2/ViewModels/AsyncControlViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mda_take_2.ViewModels
+{
+    internal class AsyncControlViewModel
+    {
+    }
+}
